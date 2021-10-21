672daf970c37491602b0d4bbf5c77b88dae3f6e9
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-21 09:32:42 +0700

    Initialize master

---
+++ handlers/userbotjoin.py
@@ -0,0 +1,135 @@
+import asyncio
+
+from callsmusic.callsmusic import client as USER
+from config import BOT_USERNAME, SUDO_USERS
+from helpers.decorators import authorized_users_only, errors
+from helpers.filters import command
+from pyrogram import Client, filters
+from pyrogram.errors import UserAlreadyParticipant
+
+
+@Client.on_message(
+    command(["join", f"join@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+)
+@authorized_users_only
+@errors
+async def addchannel(client, message):
+    chid = message.chat.id
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "• **i'm not have permission:**\n\n» ❌ __Add Users__",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "music assistant"
+
+    try:
+        await USER.join_chat(invitelink)
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            f"✅ **userbot already entered chat**",
+        )
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"🛑 Flood Wait Error 🛑 \n\n**userbot couldn't join your group due to heavy join requests for userbot**"
+            "\n\n**or add assistant manually to your Group and try again**",
+        )
+        return
+    await message.reply_text(
+        f"✅ **userbot succesfully entered chat**",
+    )
+
+
+@Client.on_message(
+    command(["leave", f"leave@{BOT_USERNAME}"]) & filters.group & ~filters.edited
+)
+@authorized_users_only
+async def rem(client, message):
+    try:
+        await USER.send_message(message.chat.id, "✅ userbot successfully left chat")
+        await USER.leave_chat(message.chat.id)
+    except:
+        await message.reply_text(
+            "❌ **userbot couldn't leave your group, may be floodwaits.**\n\n**or manually kick userbot from your group**"
+        )
+
+        return
+
+
+@Client.on_message(command(["leaveall", f"leaveall@{BOT_USERNAME}"]))
+async def bye(client, message):
+    if message.from_user.id not in SUDO_USERS:
+        return
+
+    left = 0
+    failed = 0
+    lol = await message.reply("🔄 **userbot** leaving all chats !")
+    async for dialog in USER.iter_dialogs():
+        try:
+            await USER.leave_chat(dialog.chat.id)
+            left += 1
+            await lol.edit(
+                f"Userbot leaving all group...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        except:
+            failed += 1
+            await lol.edit(
+                f"Userbot leaving...\n\nLeft: {left} chats.\nFailed: {failed} chats."
+            )
+        await asyncio.sleep(0.7)
+    await client.send_message(
+        message.chat.id, f"Left from {left} chats.\nFailed in {failed} chats."
+    )
+
+
+@Client.on_message(
+    command(["joinchannel", "ubjoinc"]) & ~filters.private & ~filters.bot
+)
+@authorized_users_only
+@errors
+async def addcchannel(client, message):
+    try:
+        conchat = await client.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply(
+            "❌ `NOT_LINKED`\n\n• **The userbot could not play music, due to group not linked to channel yet.**"
+        )
+        return
+    try:
+        invitelink = await client.export_chat_invite_link(chid)
+    except:
+        await message.reply_text(
+            "• **i'm not have permission:**\n\n» ❌ __Add Users__",
+        )
+        return
+
+    try:
+        user = await USER.get_me()
+    except:
+        user.first_name = "helper"
+
+    try:
+        await USER.join_chat(invitelink)
+    except UserAlreadyParticipant:
+        await message.reply_text(
+            "<b>✅ userbot already joined channel</b>",
+        )
+        return
+    except Exception as e:
+        print(e)
+        await message.reply_text(
+            f"<b>🛑 Flood Wait Error 🛑\n\n**userbot couldn't join to channel** due to heavy join requests for userbot, make sure userbot is not banned in channel."
+            f"\n\nor manually add @{ASSISTANT_NAME} to your channel and try again</b>",
+        )
+        return
+    await message.reply_text(
+        "<b>✅ userbot successfully joined channel</b>",
+    )
